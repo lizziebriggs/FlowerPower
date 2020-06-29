afc4925d583c0ee2896fce151618e9fd8ee1b9cb
Lizzie <lizzie.briggs10@gmail.com>
2020-06-29 15:40:31 +0100

    Created flower skeleton to produce pollen idle and on tap
    Flower can also be levelled up which increases pollen produced

---
+++ Flower Power/Assets/Scripts/Flowers/Flower.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Flowers
+{
+    [CreateAssetMenu(fileName = "New Flower", menuName = "Flower/New Flower")]
+    
+    [System.Serializable]
+    public class Flower : ScriptableObject
+    {
+        public int startingPollen;
+        public int pollenIncrease;
+        public Sprite flowerSprite;
+    }
+}
