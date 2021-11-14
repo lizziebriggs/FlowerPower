de1514fcb4ecc295c6f30a707d570743b6b153d9
librasaur <lizzie.briggs10@gmail.com>
2021-11-14 13:30:11 +0000

    Improved UI and attempted animation

--- Flower Power/Assets/Scripts/GUI/GuiBase.cs
+++ Flower Power/Assets/Scripts/GUI/GuiBase.cs
@@ -5,20 +5,21 @@ namespace GUI
     public class GuiBase : MonoBehaviour
     {
         [SerializeField] private Animator animator;
+        [SerializeField] private bool pause;
         [SerializeField] private GameObject greyOut;
         private static readonly int IsOpen = Animator.StringToHash("IsOpen");
 
         public void Show()
         {
-            Time.timeScale = 0;
-            greyOut.SetActive(true);
+            if(pause) Time.timeScale = 0;
+            if(greyOut) greyOut.SetActive(true);
             animator.SetBool(IsOpen, true);
         }
 
         public void Hide()
         {
-            Time.timeScale = 1;
-            greyOut.SetActive(false);
+            if(pause) Time.timeScale = 1;
+            if(greyOut) greyOut.SetActive(false);
             animator.SetBool(IsOpen, false);
         }
     }
